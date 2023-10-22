cf6b3031a3e4f5e1f1048a4478822c2e48a1f0f7
jakob <jakobhkrohn@gmail.com>
2023-10-22 22:18:16 +0200

    enter text on enter key

--- script.js
+++ script.js
@@ -4,5 +4,14 @@ function enter_button_function() {
     text_box.innerHTML = "Nothing you fool";
 }
 
+var question_box = document.getElementById("question_box_id");
+question_box.addEventListener("keyup", function (e){
+    // todo: use something that is not deprecated. 
+    if (e.which === 13){
+        enter_button_function();
+    }
+});
+
+
 var button = document.getElementById("enter_button_id");
 button.onclick = enter_button_function;
