649e451a6d002423a3cb5437b0d66a158818fc20
jakob <jakobhkrohn@gmail.com>
2023-10-22 21:55:36 +0200

    Giving the baduskha a default answer

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function enter_button_function() {
+    var text_box = document.getElementById("baduskha_answer_id");
+    text_box.innerHTML = "Nothing you fool";
+}
+
+var button = document.getElementById("enter_button_id");
+button.onclick = enter_button_function;
